21e2159f93f6bb8c4aee9a0d3a739ef1a366d2b4
xiao <renxiao@zhiwyl.com>
2023-01-06 10:50:44 +0800

    feat: 替换路由类似傻妞

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -65,9 +65,9 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
     /** 此方法会跳转到 redirect 参数所在的位置 */
     const redirect = urlParams.get('redirect');
     // Note: There may be security issues, please note
-    if (window.location.pathname !== '/user/login' && !redirect) {
+    if (window.location.pathname !== '/admin/user/login' && !redirect) {
       history.replace({
-        pathname: '/user/login',
+        pathname: '/admin/user/login',
         search: stringify({
           redirect: pathname + search,
         }),
